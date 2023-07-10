8c7966766ba0a79810a0069c6771b52ff4962231
menhunging <menhunging@gmail.com>
2023-07-10 11:17:34 +0300

    finish contacts

--- js/script.js
+++ js/script.js
@@ -483,6 +483,42 @@ $(document).ready(function () {
     });
   }
 
+  if ($(".counter-box").length > 0) {
+    let show = true;
+    let countbox = ".counter-box";
+
+    $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      var w_height = $(window).height(); // Высота окна браузера
+      var d_height = $(document).height(); // Высота всего документа
+      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (
+        w_top + 500 >= e_top ||
+        w_height + w_top == d_height ||
+        e_height + e_top < w_height
+      ) {
+        $(".benefits__number").css("opacity", "1");
+        $(".about-numbers__list .num").spincrement({
+          thousandSeparator: "",
+          duration: 3000,
+        });
+
+        show = false;
+      }
+    });
+  }
+
+  if ($(".city-bl").length > 0) {
+    $(".city-bl").on("click", function (event) {
+      event.preventDefault();
+
+      $(".list-cites .btn").removeClass("show");
+      $(this).next(".btn").addClass("show");
+    });
+  }
+
   if ($("#lottie-1").length > 0) {
     let number = 1000;
 
